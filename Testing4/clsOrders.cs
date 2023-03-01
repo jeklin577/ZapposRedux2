a564f81e7eaba31dda718c01d5f5d953ade8f86b
Harry Pape <harry.pape17@gmail.com>
2023-03-01 11:55:25 +0000

    Added the test methods to the testing class

--- Testing4/clsOrders.cs
+++ Testing4/clsOrders.cs
@@ -1,14 +1,15 @@
 ﻿using System;
 
-namespace Testing1
+namespace Testing4
 {
-    public class clsCustomer
+    public class clsOrders
     {
         public int orderID {get; set;}
         public int  customerID {get; set;}
         public string itemNames {get; set; }
         public int itemQuantity {get; set; }
         public DateTime dateAdded {get; internal set; }
+        public string deliveryAddress { get; set; }
         public bool readyToDispatch {get; internal set; }
     }
 }
--- Testing4/tstOrders.cs
+++ Testing4/tstOrders.cs
@@ -6,15 +6,74 @@ namespace Testing4
     [TestClass]
     public class tstOrders
     {
+
         [TestMethod]
-        public void TestMethod1()
+        public void OrderIdTest()
         {
-            clsOrders anOrder = new clsOrders();
-            Assert.IsNotNull(anOrder);
+            //create an instance of the class we want to create
+            clsOrders orderIdTest= new clsOrders();
+            //create some test date to assign to the property
+            int TestData = 001;
+            //assign the data to the property
+            orderIdTest.orderID = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(orderIdTest.orderID, TestData);
+        }
+
+        [TestMethod]
+        public void CustomerIdTest()
+        {
+            clsOrders customerIdTest = new clsOrders();
+            Int32 TestData = 1533;
+            customerIdTest.customerID = TestData;
+            Assert.AreEqual(customerIdTest.customerID, TestData);
+        }
+
+        [TestMethod]
+        public void ItemNameTest()
+        {
+            clsOrders itemNameTest = new clsOrders();
+            string TestData = "Jordans";
+            itemNameTest.itemNames= TestData;
+            Assert.AreEqual(itemNameTest.itemNames, TestData);
+        }
+
+        [TestMethod]
+        public void ItemQuantityTest()
+        {
+            clsOrders itemQuantityTest = new clsOrders();
+            Int32 TestData = 3;
+            itemQuantityTest.itemQuantity = TestData;
+            Assert.AreEqual(itemQuantityTest.itemQuantity, TestData);
+        }
+
+        [TestMethod]
+        public void DateAddedTest()
+        {
+            clsOrders dateAddedTest= new clsOrders();
+            DateTime TestData = DateTime.Now.Date;
+            dateAddedTest.dateAdded= TestData;
+            Assert.AreEqual(dateAddedTest.dateAdded, TestData);
+        }
+
+        [TestMethod]
+        public void DeliveryAddressTest()
+        {
+            clsOrders deliveryAddressTest = new clsOrders();
+            string TestData = "110 Menlo Park";
+            deliveryAddressTest.deliveryAddress = TestData;
+            Assert.AreEqual(deliveryAddressTest.deliveryAddress, TestData);
+        }
+
+        [TestMethod]
+        public void ReadyToDispatchTest()
+        {
+            clsOrders readyToDispatchTest = new clsOrders();
+            bool TestData = true;
+            readyToDispatchTest.readyToDispatch= TestData;
+            Assert.AreEqual(readyToDispatchTest.readyToDispatch, TestData);
         }
     }
 
-    class clsOrders
-    {
-    }
+
 }
