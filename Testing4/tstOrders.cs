e13ad69d839c714d5679cb2eae66eb3d376f62d8
Harry Pape <harry.pape17@gmail.com>
2023-03-01 10:47:53 +0000

    Added the test class and the table attributes

---
+++ Testing4/tstOrders.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing4
+{
+    [TestClass]
+    public class tstOrders
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            clsOrders anOrder = new clsOrders();
+            Assert.IsNotNull(anOrder);
+        }
+    }
+
+    class clsOrders
+    {
+    }
+}
