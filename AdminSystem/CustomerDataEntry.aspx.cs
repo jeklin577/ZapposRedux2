c3d447e5c0a7bfd26076de7496127c13a39f8d1b
Sean Walsh <P2666656@my365.dmu.ac.uk>
2023-03-02 01:16:17 +0000

    Added properly working UI to Data Entry and viewer pages for Customer

--- AdminSystem/CustomerDataEntry.aspx.cs
+++ AdminSystem/CustomerDataEntry.aspx.cs
@@ -4,6 +4,11 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
+using Testing1; /// <summary>
+/// This is probably dumb and bad for a myriad of reasons but despite having clsCustomer literally inside the class library folder, using ClassLibrary; just isn't recognizing the clsCustomer class
+/// So i'm doing this, i'm also pretty sure we previously had our clsCustomer class file in the wrong location, so we moved that to it's home in the classlibrary.
+/// </summary>
 
 public partial class _1_DataEntry : System.Web.UI.Page
 {
@@ -15,6 +20,21 @@ public partial class _1_DataEntry : System.Web.UI.Page
     protected void btnOK_Click(object sender, EventArgs e)
     {
         ///Redirects user to the CustomerViewer page
+        ///Response.Redirect("CustomerViewer.aspx");
+        ///
+        clsCustomer CustomerInfo = new clsCustomer();
+        CustomerInfo.Username = txtUsername.Text;
+        CustomerInfo.Password = txtPassword.Text;
+        CustomerInfo.ShippingAddress = txtShippingAddress.Text;
+        ///Creates an instance of a customer, and then populates it's three varchar fields with the text contained in the text boxes we've added to the site.
+        ///
+        Session["CustomerInfo"] = CustomerInfo;
+        ///Alright vague networking memories here, Session's are used as temporary files that allow for sites to remember stuff like, say, the info inputted into our data entry page, and use them
+        ///on, say, our viewer page.
+        ///
         Response.Redirect("CustomerViewer.aspx");
+        ///Same as last time, just redirects to the CustomerViewer page.
+
+
     }
 }
---
+++ ClassLibrary/clsCustomer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Testing1
+{
+    public class clsCustomer
+    {
+        public string Username { get; set;}
+
+        public bool HasOrder {get; set;}
+        public string Password { get; set; }
+        public DateTime DateAdded { get; set; }
+        public string ShippingAddress { get; set; }
+        public int CustomerID { get; set; }
+    }
+}
