1791659e64d10a758c06903ec4fcbf5c10399950
p2670810 <p2670810@GWHXX474WIN013W>
2023-03-06 11:54:25 +0000

    Todays Work

---
+++ Testing3/tstSupplier.cs
@@ -0,0 +1,28 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing3
+{
+    [TestClass]
+    public class tstSupplier
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create an instance of the class we want to create 
+            clsSupplier AnSupplier = new clsSupplier();
+            //test to see thta it exists
+            Assert.IsNotNull(AnSupplier);
+        }
+          [TestMethod]
+          public void SupplierIDPropertyOK()
+        { 
+          clsSupplier clsSupplier = new clsSupplier();
+            Int32 TestData = 1;
+            AnSupplier.SupplierID = TestData;
+            Assert.AreEqual(AnSupplier.SupplierID, TestData);
+            }
+
+    }
+}
