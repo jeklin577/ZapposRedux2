efdb7d5a06d9fd71ec858cbff4ade79000d56911
P2670902 <P2670902@my365.dmu.ac.uk>
2023-03-14 00:02:11 +0000

    Completed  a sample of the presention layer

--- AdminSystem/StockViewer.aspx.cs
+++ AdminSystem/StockViewer.aspx.cs
@@ -4,11 +4,17 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
-public partial class _1Viewer : System.Web.UI.Page
+public partial class StockViewer : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        //create a new instance of clsStock
+        clsStock TestStock = new clsStock();
+        //get the data from the session object
+        TestStock = (clsStock)Session["TestStock"];
+        //display the Sneaker ID for this entry
+        Response.Write(TestStock.SneakerID);
     }
 }
