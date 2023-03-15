771c66c01e1efe0492039bdc17bac6ec110b74df
Harry Pape <harry.pape17@gmail.com>
2023-03-15 09:48:34 +0000

    Added Find Method

--- ClassLibrary/clsOrders.cs
+++ ClassLibrary/clsOrders.cs
@@ -4,13 +4,32 @@ namespace ClassLibrary
 {
     public class clsOrders
     {
-     public int orderID { get; set; }
+        //private data member for the orderNo property
+        private Int32 mOrderNo;
+
+     public int orderID 
+        {
+            get
+            {
+                return mOrderNo;
+            }
+            set
+            {
+                mOrderNo = value;
+            }
+        }
      public int customerID { get; set; }
      public string itemNames { get; set; }
      public int itemQuantity { get; set; }
      public DateTime dateAdded { get; set; }
      public string deliveryAddress { get; set; }
      public bool readyToDispatch { get; set; }
+
+
+    public bool Find(int OrderNo)
+        {
+            return true;
+        }
      
     }
 }
