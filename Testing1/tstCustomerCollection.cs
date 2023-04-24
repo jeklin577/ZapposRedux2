8a7e9684e8c079b5439e8ad1dad5663e47b3c206
Sean Walsh <P2666656@my365.dmu.ac.uk>
2023-04-24 15:46:26 +0100

    Still Unfinished, having trouble with connecting to the SQL server

--- Testing1/tstCustomerCollection.cs
+++ Testing1/tstCustomerCollection.cs
@@ -20,9 +20,19 @@ namespace ClassLibrary
 
         [TestMethod]
 
-     //   public void CustomerListOK()
-       // {
-            /*
+        public void CountPropertyOK()
+        {
+            clsCustomerCollection AllCustomers = new clsCustomerCollection();
+            Int32 SomeCount = 2;
+            AllCustomers.Count = SomeCount;
+            Assert.AreEqual(AllCustomers.Count, SomeCount);
+        }
+
+        [TestMethod]
+
+        public void CustomerListOK()
+        {
+            
             clsCustomerCollection AllCustomers = new clsCustomerCollection();
 
             List<clsCustomer> TestList = new List<clsCustomer>();
@@ -39,8 +49,41 @@ namespace ClassLibrary
             TestList.Add(TestItem);
             AllCustomers.CustomerList = TestList;
 
-            Assert.AreEqual(AllCustomers, TestList);
-            \*
+            Assert.AreEqual(AllCustomers.CustomerList, TestList);
+            
+        }
+
+        [TestMethod]
+
+        public void ListAndCountOK()
+        {
+            clsCustomerCollection AllCustomers = new clsCustomerCollection();
+
+            List<clsCustomer> TestList = new List<clsCustomer>();
+            clsCustomer TestItem = new clsCustomer();
+
+
+            TestItem.HasOrder = true;
+            TestItem.ShippingAddress = "123 Street Street";
+            TestItem.DateAdded = DateTime.Now.Date;
+            TestItem.Password = "Password";
+            TestItem.Username = "Dave";
+            TestItem.CustomerID = 5;
+
+            TestList.Add(TestItem);
+            AllCustomers.CustomerList = TestList;
+
+            Assert.AreEqual(AllCustomers.Count, TestList.Count);
+        }
+
+        [TestMethod]
+
+        public void TwoRecordsPresent()
+        {
+            clsCustomerCollection AllCustomers = new clsCustomerCollection();
+
+            Assert.AreEqual(AllCustomers.Count, 2);
+
         }
 
 
