527d5f66c4032dcf7c6f07e0a97740aa4925bbed
Sean Walsh <P2666656@my365.dmu.ac.uk>
2023-04-24 19:41:05 +0100

    TODO: Add, update, filter, delete

--- Testing1/tstCustomerCollection.cs
+++ Testing1/tstCustomerCollection.cs
@@ -18,21 +18,24 @@ namespace ClassLibrary
 
         }
 
-        [TestMethod]
+        /// [TestMethod]
 
-        public void CountPropertyOK()
-        {
-            clsCustomerCollection AllCustomers = new clsCustomerCollection();
-            Int32 SomeCount = 2;
-            AllCustomers.Count = SomeCount;
-            Assert.AreEqual(AllCustomers.Count, SomeCount);
-        }
+        ///public void CountPropertyOK()
+        ///{
+        ///    clsCustomerCollection AllCustomers = new clsCustomerCollection();
+        ///     Int32 SomeCount = 2;
+        ///     AllCustomers.Count = SomeCount;
+        ///     Assert.AreEqual(AllCustomers.Count, SomeCount);
+        /// }
+        /// This test is superceded by the ListAndCountOK test, preserved here, as an example of hardcoding in data instead of writing an actually effective test.
+        /// 
+
 
         [TestMethod]
 
         public void CustomerListOK()
         {
-            
+
             clsCustomerCollection AllCustomers = new clsCustomerCollection();
 
             List<clsCustomer> TestList = new List<clsCustomer>();
@@ -50,7 +53,7 @@ namespace ClassLibrary
             AllCustomers.CustomerList = TestList;
 
             Assert.AreEqual(AllCustomers.CustomerList, TestList);
-            
+
         }
 
         [TestMethod]
@@ -76,17 +79,23 @@ namespace ClassLibrary
             Assert.AreEqual(AllCustomers.Count, TestList.Count);
         }
 
+        ///  [TestMethod]
+
+        /// public void TwoRecordsPresent()
+        /// {
+        ///     clsCustomerCollection AllCustomers = new clsCustomerCollection();
+
+        ///     Assert.AreEqual(AllCustomers.Count, 2);
+
+        /// }
+        /// See the description for the CountPropertyOK method, same situation applies here.
+
         [TestMethod]
 
-        public void TwoRecordsPresent()
+        public void AddMethodOK();
         {
-            clsCustomerCollection AllCustomers = new clsCustomerCollection();
-
-            Assert.AreEqual(AllCustomers.Count, 2);
 
         }
-
-
 
     }
 }
