773b4b1997e3fbfb7710a6d0c5f9e3bcadc10c66
Sean Walsh <P2666656@my365.dmu.ac.uk>
2023-04-26 11:49:51 +0100

    Need to restart VS

--- AdminSystem/CustomerConfirmDelete.aspx.cs
+++ AdminSystem/CustomerConfirmDelete.aspx.cs
@@ -4,11 +4,32 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1_ConfirmDelete : System.Web.UI.Page
 {
+    Int32 CustomerID; //"CustomerID is a number, and it exists"
     protected void Page_Load(object sender, EventArgs e)
     {
+        CustomerID = Convert.ToInt32(Session["CustomerID"]);
 
+    }
+
+    ///protected void TextBox1_TextChanged(object sender, EventArgs e)
+    // {
+    //
+    // }
+
+    protected void btnYes_Click(object sender, EventArgs e)
+    {
+        clsCustomerCollection CustomerList = new clsCustomerCollection();
+        CustomerList.ThisCustomer.Find(CustomerID);
+        CustomerList.Delete();
+        Response.Redirect("CustomerList.aspx");
+    }
+
+    protected void btnNo_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("CustomerList.aspx");
     }
 }
